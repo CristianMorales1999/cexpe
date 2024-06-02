4148898a084f824b55c38ff3c31d3a1bc56f2ea8
CristianMorales1999 <cm9225064@gmail.com>
2024-06-02 18:40:02 -0500

    14. Creando archivo partials/nav.blade.php

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -17,15 +17,8 @@
 <body>
     <nav>
         <table class="table table-bordered">
-            <tr>
-                <th scope="col" class="{{setActivo('inicio')}}"><a href="/">Inicio</a></th>
-                <th scope="col" class="{{setActivo('programas.index')}}"><a href="programas">Programas</a></th>
-                <th scope="col" class="{{setActivo('talleres')}}"><a href="talleres">Talleres</a></th>
-                <th scope="col" class="{{setActivo('asesoramiento')}}"><a href="asesoramiento">Asesoramiento</a></th>
-                <th scope="col" class="{{setActivo('contacto')}}"><a href="contacto">Contacto</a></th>
-            </tr>
+            @include('partials.nav')
         </table>
     </nav>
-    @yield('content')
 </body>
 </html>
---
+++ resources/views/partials/nav.blade.php
@@ -0,0 +1,14 @@
+<thead class="table table-bordered">
+    <tr>
+        <th scope="col" class="{{setActivo('inicio')}}"><a href="/">Inicio</a></th>
+        <th scope="col" class="{{setActivo('programas.index')}}"><a href="programas">Programas</a></th>
+        <th scope="col" class="{{setActivo('talleres')}}"><a href="talleres">Talleres</a></th>
+        <th scope="col" class="{{setActivo('asesoramiento')}}"><a href="asesoramiento">Asesoramiento</a></th>
+        <th scope="col" class="{{setActivo('contacto')}}"><a href="contacto">Contacto</a></th>
+    </tr>
+    <tr>
+        <td colspan="5">
+            @yield('content')
+        </td>
+    </tr>
+</thead>
