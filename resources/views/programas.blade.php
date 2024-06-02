13bd6336f363b0c9c0460f2472fb5a55baed8447
CristianMorales1999 <cm9225064@gmail.com>
2024-06-02 16:56:09 -0500

    5. Estructuras de control en Blade

--- resources/views/programas.blade.php
+++ resources/views/programas.blade.php
@@ -4,4 +4,13 @@
  
 @section('content')
     <h2>Programas</h2>
+    <ul>
+        @if($programas)
+            @foreach($programas as $programa)
+                <li>{{$programa['nombre']}}</li>
+            @endforeach
+        @else
+            <li>Actualmente no se cuenta con ningún programa disponible</li>
+        @endif
+    </ul>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,15 @@
 use Illuminate\Support\Facades\Route;
 
 
+$programas=[
+    ['nombre'=>'Danza'],
+    ['nombre'=>'Baile'],
+    ['nombre'=>'Oratoria'],
+    ['nombre'=>'Liderazgo']
+];
+
 Route::view('/','inicio')->name('inicio');
-Route::view('programas','programas')->name('programas');
+Route::view('programas','programas',compact('programas'))->name('programas');
 Route::view('talleres','talleres')->name('talleres');
 Route::view('asesoramiento','asesoramiento')->name('asesoramiento');
 Route::view('contacto','contacto')->name('contacto');
