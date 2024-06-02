b86d315050ccccbe18378bcc5e960946ed75a4e9
CristianMorales1999 <cm9225064@gmail.com>
2024-06-02 17:29:26 -0500

    8. Generamos las rutas de los métodos del Controlador

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,8 @@ Route::get('programas','App\Http\Controllers\ProgramasController@index')->name('
 Route::view('talleres','talleres')->name('talleres');
 Route::view('asesoramiento','asesoramiento')->name('asesoramiento');
 Route::view('contacto','contacto')->name('contacto');
+
+//Generamos las 7 rutas de los métodos del controlador
+//Si quereremos verificar la creación de estas rutas hacemos uso del siguiente comando en la terminal:
+    //php artisan route:list
+Route::resource('programas','App\Http\Controllers\ProgramasController');
