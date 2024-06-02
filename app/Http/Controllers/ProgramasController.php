68fb979cb3a7140a0f6d928be9c809116783b2ed
CristianMorales1999 <cm9225064@gmail.com>
2024-06-02 17:13:38 -0500

    6. Creación de controladores

---
+++ app/Http/Controllers/ProgramasController.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * - Para crear un controlado desde la terminal tenemos 2 opciones:
+ * 
+ * Comando 1 (El cual crea consigo los 7 metodos del controlador): 
+ * php artisan make:controller ProgramasController --resource
+ * 
+ * Comando 2(El cual crea el controlador pero sin ningun metodo):
+ * php artisan make:controller ProgramasController
+ * 
+ * 
+ * - Adicional a esto, si queremos listar las rutas existentes desde la terminal, para luego ver los controladores usamos:
+ * 
+ * php artisan route:list
+ * 
+ */
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProgramasController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
